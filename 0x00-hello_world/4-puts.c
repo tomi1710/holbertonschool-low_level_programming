14ea1df8ac6c2c2e17f1fc026757a5cdfd21d8b7
tomi1710 <2294@holbertonschool.com>
2020-09-16 16:48:20 +0000

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
-#include <string.h>
 
 int main()
 {
-char str1[15];
-
-strcpy(str1, "\"Programming is like building a multilingual puzzle\n");
-
-puts(str1);
+puts("\"Programming is like building a multilengual puzzle");
 
 return(0);
     }
