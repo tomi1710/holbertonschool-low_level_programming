7620c261841d864d7c0300df6a5c39ac52d568d2
tomi1710 <2294@holbertonschool.com>
2020-09-16 16:58:11 +0000

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main()
 {
 puts("\"Programming is like building a multilengual puzzle\n");
 
-getchar();
 return 0;
     }
