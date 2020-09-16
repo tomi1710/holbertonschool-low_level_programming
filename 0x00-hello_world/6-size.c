e39299a1e4a833f8a9d1ac4dfd46481d9adf1655
tomi1710 <2294@holbertonschool.com>
2020-09-16 18:11:39 +0000

    a

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ long int longinttype;
 long long int longlonginttype;
 float floattype;
 
-printf("Size of a char: %ls byte(s)\n", sizeof(chartype));
-printf("Size of an int: %ls byte(s)\n", sizeof(inttype));
-printf("Size of a long int: %ls byte(s)\n", sizeof(longinttype));
-printf("Size of a long long int: %ls byte(s)\n", sizeof(longlonginttype));
-printf("Size of a float: %ls byte(s)\n", sizeof(floattype));
+printf("Size of a char: %i byte(s)\n", sizeof(chartype));
+printf("Size of an int: %i byte(s)\n", sizeof(inttype));
+printf("Size of a long int: %i byte(s)\n", sizeof(longinttype));
+printf("Size of a long long int: %i byte(s)\n", sizeof(longlonginttype));
+printf("Size of a float: % byte(s)\n", sizeof(floattype));
 
 return (0);
 }
