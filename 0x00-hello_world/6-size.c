545650d254aeec0bbd5aca201a7cef9aa51d04a4
tomi1710 <2294@holbertonschool.com>
2020-09-16 18:03:08 +0000

    a

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,15 +4,15 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main()
 {
-  //adadad
+
 char chartype;
 int inttype;
 long int longinttype;
 long long int longlonginttype;
 float floattype;
-//aaaa
+
 printf("Size of a char: %ls byte(s)\n", sizeof(chartype));
 printf("Size of an int: %ls byte(s)\n", sizeof(inttype));
 printf("Size of a long int: %ls byte(s)\n", sizeof(longinttype));
