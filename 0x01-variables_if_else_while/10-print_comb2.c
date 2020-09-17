7b41484f89da27501a79e80e026101b3290102f4
tomi1710 <2294@holbertonschool.com>
2020-09-17 20:05:06 +0000

    a

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -11,8 +11,8 @@ int num;
 
 for (num = 0 ; num < 100 ; num++)
 {
-putchar((num/10)+ '0');
-putchar((num%10)+ '0');
+putchar((num / 10) + '0');
+putchar((num % 10) + '0');
 if (num != 99)
 {
 putchar(',');
