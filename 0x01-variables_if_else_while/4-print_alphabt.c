c81903bd37b6730466ffb910e4e0d83069b05930
tomi1710 <2294@holbertonschool.com>
2020-09-17 17:13:31 +0000

    a

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,10 +8,13 @@ int main(void)
 {
 char ch;
 
-for (ch = "a" ; ch <= "d" ; ch = "f" ; ch <= "p" ; ch = "r" ; ch <= "z" ; ch++)
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+if (ch != 'q' && ch != 'e')
 {
 putchar(ch);
 }
-
+}
+putchar('\n')
 return (0);
 }
