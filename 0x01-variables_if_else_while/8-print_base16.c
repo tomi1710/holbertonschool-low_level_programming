91748f612e2b507096c9fdb2d05c1c3b660badfe
tomi1710 <2294@holbertonschool.com>
2020-09-17 19:06:46 +0000

    a

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,11 +8,11 @@ int main(void)
 {
 int num;
 char ch;
-for (num = '0' ; num <= '9' ; num++)
+for (num = '0'; num <= '9'; num++)
 {
 putchar(num);
 }
-for (ch = 'a' ; ch <= 'f' ; ch++)
+for (ch = 'a'; ch <= 'f'; ch++)
 {
 putchar(ch);
 }
