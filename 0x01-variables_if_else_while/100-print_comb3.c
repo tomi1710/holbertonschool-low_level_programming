08ba3c4ecddec9af8d6831f9e197d3c5b373dcc9
tomi1710 <2294@holbertonschool.com>
2020-09-17 20:52:08 +0000

    a

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+int num, num1;
+
+for (num = '0' ; num <= '9' ; num++)
+{
+for (num1 = '0' ; num1 <= '9' ; num1++)
+{
+if (num == num1)
+{}
+else if (num > num1)
+{}
+else 
+{
+putchar(num);
+putchar(num1);
+if (num != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+
+return (0);
+}
