d43ec87ab7c03aae5f578e1b869eb082b1b3a31a
tomi1710 <2294@holbertonschool.com>
2020-09-17 16:18:28 +0000

    a

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ if (lastdigit > 5)
 {
 printf("Last digit of %i is %i and is greater than 5\n", n, lastdigit);
 }
-else if (lastdigit < 6)
+else if (lastdigit < 6 && lastdigit != 0)
 {
 printf("Last digit of %i is %i and is less than 6 and not 0\n", n, lastdigit);
 }
