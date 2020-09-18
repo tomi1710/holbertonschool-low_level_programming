2b23149811446e87e2bd76e8f62ad5c0b1805679
tomi1710 <2294@holbertonschool.com>
2020-09-18 22:31:22 +0000

    a

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,37 +9,28 @@ int main(void)
 
 int num, num1, num2, num3;
 
-for (num = '0' ; num <= '9' ; num++)
+for (num = '0'; num <= '9'; num++)
 {
-for (num1 = '0' ; num1 <= '9' ; num1++)
+for (num1 = '0'; num1 <= '9'; num1++)
 {
-for (num2 = '0' ; num2 <= '9' ; num2++)
+for (num2 = '0'; num2 <= '9'; num2++)
 {
-for (num3 = '0' ; num3 <= '9' ; num3++)
+for (num3 = '0'; num3 <= '9'; num3++)
 {
-if (num > num2)
-{}
- num3 = 0;
-do {
-       if (num == num2 && num1 == num3)
-	 {}
-else
+if (num < num2 || (num == num2 && num1 < num3))
 {
 putchar(num);
 putchar(num1);
 putchar(' ');
 putchar(num2);
 putchar(num3);
-if (num == '9' && num1 == '8')
-{}
-else
+if (num != '9' || num1 != '8' || num2 != '9' || num3 != '9')
 {
 putchar(',');
 putchar(' ');
 }
 }
-       num3++;
-     } while (num3 == 9);
+}
 }
 }
 }
