e7a7a4bf5ce5ad1534f27ba5576ece0e92dde789
tomi1710 <2294@holbertonschool.com>
2020-09-18 13:34:04 +0000

    a

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+int num, num1, num2, num3;
+
+for (num = '0' ; num <= '9' ; num++)
+{
+for (num1 = '0' ; num1 <= '9' ; num1++)
+{
+for (num2 = '0' ; num2 <= '9' ; num2++)
+{
+for (num3 = '0' ; num3 <= '9' ; num3++)
+{
+if (num == num2 && num1 == num3)
+{}
+else if (num > num2)
+{}
+else
+{
+putchar(num);
+putchar(num1);
+putchar(' ');
+putchar(num2);
+putchar(num3);
+if (num == 9 && num1 == 8 && num2 == 9 && num3 == 9)
+{}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+}
+putchar('\n');
+
+return (0);
+}
