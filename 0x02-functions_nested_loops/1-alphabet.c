5228d5179ec1051dd69ad61b6ae9d6dc423646bb
tomi1710 <2294@holbertonschool.com>
2020-09-22 16:07:32 +0000

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char cha;
+
+for (cha == 'a' ; cha <= 'z' ; cha++)
+
+_putchar('cha');
+
+return (0);
+}
