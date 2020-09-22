082b99e7bb0e8813b0f4009ee01ba7df6c2b9153
tomi1710 <2294@holbertonschool.com>
2020-09-22 19:48:02 +0000

    a

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ *_islower - alalalalala
+ *
+ *jajajajaj
+ *
+ */
+int _islower(int c)
+{
+
+if (c > 96)
+{
+if (c < 126)
+{
+return (1);
+}
+else
+{
+return (1);
+}
+}
+else
+{
+return (0);
+}
+}
