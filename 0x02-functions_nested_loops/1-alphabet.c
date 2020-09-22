a4e491e49a9a79606055fd56d19a48c4b02e0d5f
tomi1710 <2294@holbertonschool.com>
2020-09-22 17:58:02 +0000

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,33 +6,7 @@
  */
 int main(void)
 {
-
-_putchar('a');
-_putchar('b');
-_putchar('c');
-_putchar('d');
-_putchar('e');
-_putchar('f');
-_putchar('g');
-_putchar('h');
-_putchar('i');
-_putchar('j');
-_putchar('k');
-_putchar('l');
-_putchar('m');
-_putchar('n');
-_putchar('o');
-_putchar('p');
-_putchar('q');
-_putchar('r');
-_putchar('s');
-_putchar('t');
-_putchar('u');
-_putchar('v');
-_putchar('w');
-_putchar('x');
-_putchar('y');
-_putchar('z');
+print_alphabet();
 
 return (0);
 }
