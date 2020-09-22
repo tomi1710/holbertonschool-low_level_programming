1c99a40e581e0110b7bd377a3ca0c4bf6cbf7631
tomi1710 <2294@holbertonschool.com>
2020-09-22 18:44:04 +0000

    a

---
+++ 0x02-functions_nested_loops/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *print_alphabet_10 - lalalalaalalalalal
+ *
+ *assdda
+ */
+void print_alphabet_x10(void)
+{
+char ch;
+int a;
+
+a = 0;
+
+while (a < 10)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+a++;
+}
+}
+}
