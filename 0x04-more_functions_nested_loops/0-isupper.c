2587a02c3f7c9a07b85f097547a38316e288f719
tomi1710 <2294@holbertonschool.com>
2020-09-24 12:36:34 +0000

    a

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *_isupper - prints 1 if c is upper case
+ *@c: lalaslda
+ *Return: returns 1 if c is upper case, else returns 0
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
