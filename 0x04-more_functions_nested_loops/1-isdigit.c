e0e78252509feddf03db73104f9a0aea4dddd7f0
tomi1710 <2294@holbertonschool.com>
2020-09-24 12:48:27 +0000

    a

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *_isdigit - checks if c is digit
+ *@c: input
+ *Return: returns 1 if c is digit, else returns 0
+ */
+int _isdigit(int c)
+{
+
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,5 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 int _isupper(int c);
+int _isdigit(int c);
 
 #endif
