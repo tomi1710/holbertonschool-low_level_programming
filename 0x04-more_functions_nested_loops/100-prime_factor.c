a309681bbce41a687ca6b66bf20307b1580b9721
tomi1710 <2294@holbertonschool.com>
2020-09-24 18:35:46 +0000

    a

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *main - addadd
+ */
+void main(void)
+{
+unsigned long int n, div, maxfact;
+div = 2;
+n = 612852475143;
+while (n != 0)
+{
+if ((n % div) != 0)
+{
+div ++;
+}
+else
+{
+maxfact = n;
+n = n / div;
+if (n == 1)
+{
+printf("%lu\n", maxfact);
+break;
+}
+}
+}
+}
