404c097d7392d58b4e0443379ab35832bf623f4f
tomi1710 <2294@holbertonschool.com>
2020-09-24 15:52:16 +0000

    a

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,30 +1,29 @@
 #include <stdio.h>
 /**
  *main - aaaa
+ *Return: lalaalal
  */
 int main(void)
 {
-
-  int a;
-
-  for (a = 1; a <= 100; a++)
-    {
-      if (((a % 3) == 0) && ((a % 5) == 0))
-	{
-	  printf("FizzBuzz ");
-	}
-      else if ((a % 3) == 0)
-	{
-	  printf("Fizz ");
-	}
-      else if ((a % 5) == 0)
-	{
-	  printf("Buzz ");
-	}
-      else
-	{
-	  printf("%i ", a);
-	}
-    }
-  putchar('\n');
+int a;
+for (a = 1; a <= 100; a++)
+{
+if (((a % 3) == 0) && ((a % 5) == 0))
+{
+printf("FizzBuzz ");
+}
+else if ((a % 3) == 0)
+{
+printf("Fizz ");
+}
+else if ((a % 5) == 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf("%i ", a);
+}
+}
+putchar('\n');
 }
