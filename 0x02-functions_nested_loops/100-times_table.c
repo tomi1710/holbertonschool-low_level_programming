8dd57846a5df586a05ef1dac6c62f6582d4ee4ff
tomi1710 <2294@holbertonschool.com>
2020-09-26 15:03:58 +0000

    a

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,54 +5,54 @@
  */
 void print_times_table(int n)
 {
-  int a, b, c, cien, diez, uno;
-  if (n > 15 || n < 0)
-    {
-    }
-  else
-    {
-  for (a = 0; a <= n; a++)
-    {
-      for (b = 0; b <= n; b++)
-	{
-	  c = a * b;
-	  cien = c / 100;
-	  diez = c / 10;
-	  diez = diez % 10;
-	  uno = c % 10;
-	  if (b == 0)
-	    {
-	      _putchar('0');
-	    }
-	  else
-	    {
-	      if (c > 99)
-		{
-		  _putchar(',');
-		  _putchar(' ');
-		  _putchar('0' + cien);
-		  _putchar('0' + diez);
-		  _putchar('0' + uno);
-		}
-	      else if (c > 9)
-		{
-		  _putchar(',');
-		  _putchar(' ');
-		  _putchar(' ');
-		  _putchar('0' + diez);
-		  _putchar('0' + uno);
-		}
-	      else
-		{
-		  _putchar(',');
-		  _putchar(' ');
-		  _putchar(' ');
-		  _putchar(' ');
-		  _putchar('0' + uno);
-		}
-	    }
-	}
-      _putchar('\n');
-    }
+int a, b, c, cien, diez, uno;
+if (n > 15 || n < 0)
+{
+}
+else
+{
+for (a = 0; a <= n; a++)
+{
+for (b = 0; b <= n; b++)
+{
+c = a * b;
+cien = c / 100;
+diez = c / 10;
+diez = diez % 10;
+uno = c % 10;
+if (b == 0)
+{
+_putchar('0');
+}
+else
+{
+if (c > 99)
+{
+_putchar(',');
+_putchar(' ');
+_putchar('0' + cien);
+_putchar('0' + diez);
+_putchar('0' + uno);
+}
+else if (c > 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + diez);
+_putchar('0' + uno);
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + uno);
+}
+}
+}
+_putchar('\n');
+}
 }
 }
