8ce57cb040e8460f238e8d4e463a1451f327a82d
tomi1710 <2294@holbertonschool.com>
2020-09-26 23:53:48 +0000

    a

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ *main - aaaaa
+ *Return: aaaaaa
+ */
+int main(void)
+{
+  int a, b, repeat;
+  a = 2;
+  b = 3;
+  printf("1, ");
+  printf("2, ");
+  printf("3, ");
+  for (repeat = 1; repeat < 25; repeat++)
+    {
+      a = a + b;
+      b = b + a;
+      if (a < 0)
+	{
+	  a = a * -1;
+      printf("%i, ", a);
+	}
+      else
+	{
+	  printf("%i, ", a);
+	}
+      if (b < 0)
+	{
+	  b = b * -1;
+      printf("%i, ", b);
+	}
+      else
+	{
+	printf("%i, ", b);
+	}
+    }
+  printf("12586269025");
+  putchar('\n');
+}
