2d08cfee3ed87c3848ad459b9eb84a98f81740a1
tomi1710 <2294@holbertonschool.com>
2020-09-28 21:36:14 +0000

    a

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *print_array - asdadsd
+ *@a: sfsnjfnesk
+ *@n: dsjfnjsfbjs
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < (n - 1); i++)
+{
+printf("%i, ", a[i]);
+}
+printf("%i\n", a[i]);
+}
