86b051159cf1c9eef84ed4b555adb6a23bd746cb
tomi1710 <2294@holbertonschool.com>
2020-09-28 21:18:27 +0000

    a

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ *puts_half - asassass
+ *@str: sadafsf
+ */
+void puts_half(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+}
+if (i % 2 != 0)
+{
+i++;
+}
+i = i / 2;
+for (i = i; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
