99cfbc59c728e954eab4dcd360f4ed07d242def5
tomi1710 <2294@holbertonschool.com>
2020-09-28 22:21:02 +0000

    a

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ *_strcpy - asasasas
+ *@dest: adawdasdaw
+ *@src: sdsdadwdd
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+for (i = 0; src[i] >= '\0'; i++)
+{
+dest[i] = src[i];
+}
+return(dest);
+}
