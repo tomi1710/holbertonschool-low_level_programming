452dab7be05ec5423a84a48bef536241395d2f89
tomi1710 <2294@holbertonschool.com>
2020-09-29 16:33:09 +0000

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * _atoi - sdfas
+ *@s:sdfasd
+ *Return: sdfasd
+ */
+int _atoi(char *s)
+{
+int i, a, b = 0, res = 0;
+for (a = 0; s[a] != '\0'; a++)
+{
+if (s[a] >= 0 && s[a] <= 9)
+{
+while(s[a] >= 0 && s[a] <= 9)
+{
+s[b] = s[a];
+a++;
+b++;
+}
+}
+}
+for (i = 0; i <= b; i++)
+{
+res = res * 10 + s[i] - '0';
+}
+return (res);
+}
