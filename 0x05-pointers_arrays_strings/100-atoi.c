00c13ffc72cf27c70e85a796633238d57bfacda8
tomi1710 <2294@holbertonschool.com>
2020-09-29 16:43:16 +0000

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,11 +6,16 @@
  */
 int _atoi(char *s)
 {
-int i, a, b = 0, res = 0;
+  int i, a, b = 0, c, res = 0;
 for (a = 0; s[a] != '\0'; a++)
 {
 if (s[a] >= 0 && s[a] <= 9)
 {
+if (s[a - 1] == '-')
+{
+s[0] = '-';
+b = 1;
+}
 while (s[a] >= 0 && s[a] <= 9)
 {
 s[b] = s[a];
@@ -19,7 +24,12 @@ b++;
 }
 }
 }
-for (i = 0; i <= b; i++)
+i = 0;
+if (s[b] == '-')
+{
+i = 1;
+}
+for (; i <= b; i++)
 {
 res = res * 10 + s[i] - '0';
 }
