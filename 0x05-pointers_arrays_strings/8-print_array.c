9c9c4a23999a6d74b2b3dfcff00400b4bc243200
tomi1710 <2294@holbertonschool.com>
2020-09-29 15:37:29 +0000

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,10 +7,17 @@
  */
 void print_array(int *a, int n)
 {
+if (n <= 0)
+{
+putchar('\n');
+}
+else
+{
 int i;
 for (i = 0; i < (n - 1); i++)
 {
 printf("%i, ", a[i]);
 }
 printf("%i\n", a[i]);
+}
 }
