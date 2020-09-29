f8672310e2fa204b16a2a956e1811cb523e17004
tomi1710 <2294@holbertonschool.com>
2020-09-29 12:51:11 +0000

    a

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 /**
- *reset_to_98.c - awdadwa
+ *reset_to_98 - awdadwa
  *@n: adsajda
  */
 void reset_to_98(int *n)
 {
 *n = 98;
+_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,15 @@
 #include "holberton.h"
-
+/**
+ *swap_int - asasasas
+ *@a: adasdad
+ *@b: asdadaad
+ */
 void swap_int(int *a, int *b)
 {
-  int a1, b1;
-
-  a1 = *a;
-  b1 = *b;
-
-  *a = b1;
-  *b = a1;
+int a1, b1;
+a1 = *a;
+b1 = *b;
+*a = b1;
+*b = a1;
+_putchar('\n');
 }
