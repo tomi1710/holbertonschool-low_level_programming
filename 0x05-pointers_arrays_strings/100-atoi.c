7f52690e9764d7b511cda40f0df1b44905787ba7
tomi1710 <2294@holbertonschool.com>
2020-09-30 12:16:37 +0000

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,7 +6,7 @@
  */
 int _atoi(char *s)
 {
-  int i, a, b = 0, res = 0, mas = 0, menos = 0;
+int i, a, b = 0, res = 0, mas = 0, menos = 0;
 for (a = 0; s[a] != '\0'; a++)
 {
 if (s[a] == '+')
@@ -18,7 +18,7 @@ else if (s[a] = '-')
 menos++;
 }
 if (s[a] >= 0 && s[a] <= 9)
-{ 
+{
 while (s[a] >= 0 && s[a] <= 9)
 {
 s[b] = s[a];
