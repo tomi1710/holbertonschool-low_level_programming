646e3b8dc03e37bb611cb70f87dc9e85cac50649
tomi1710 <2294@holbertonschool.com>
2020-10-01 12:50:28 +0000

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ *_strcat - adawdad
+ *@dest: adsdawd
+ *@src: adawad
+ *Return: adawda
+ */
+char *_strcat(char *dest, char *src)
+{
+int i, i2, a;
+for (i = 0; dest[i] != '\0'; i++)
+{
+}
+for (i2 = 0; src[i2] != '\0'; i2++)
+{
+}
+for (a = 0; a != i2; a++)
+{
+dest[i] = src[a];
+i++;
+}
+dest[i + 1] = '\0';
+return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,5 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+char *_strcat(char *dest, char *src);
+
+#endif
