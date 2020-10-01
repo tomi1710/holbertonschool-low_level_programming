a32c26beeeb65408b904aa3e0c1e1468c9d96e9a
tomi1710 <2294@holbertonschool.com>
2020-10-01 18:11:16 +0000

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,5 @@
+#include "holberton.h"
+
+char *cap_string(char *s)
+{
+
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ *leet - ddddddd
+ *@s: asdadasfs
+ *Return: adsfafafds
+ */
+char *leet(char *s)
+{
+int i, i2;
+char s2[20] = {"a4A4e3E3o0O0t7T7l1L1"};
+for (i = 0; s[i] != '\0'; i++)
+{
+for (i2 = 0; s2[i2] != '\0'; i2 = i2 + 2)
+{
+if (s[i] == s2[i2])
+{
+s[i] = s2[i2 + 1];
+}
+}
+}
+return(s);
+}
