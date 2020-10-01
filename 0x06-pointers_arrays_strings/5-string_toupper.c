00f1e699128ff3a63d618ae559108ad1a71d1af4
tomi1710 <2294@holbertonschool.com>
2020-10-01 17:08:53 +0000

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,8 +1,18 @@
 #include "holberton.h"
-
+/**
+ *string_toupper - dadqda
+ *@s: faeaffaw
+ *Return: faefdfaw
+ */
 char *string_toupper(char *s)
 {
-  int i;
-
-  for (i = 0; s[i] != '\0'; i++)
-    {
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] >= 97 && s[i] <= 122)
+{
+s[i] = s[i] - 32;
+}
+}
+return (s);
+}
