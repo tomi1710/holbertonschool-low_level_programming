cf3b0f6d9aca8ad12b76392766142f57d3db5c1d
tomi1710 <2294@holbertonschool.com>
2020-10-05 14:48:04 +0000

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ *_strpbrk - asdasdad
+ *@s: asadasd
+ *@accept: asdasdda
+ *Return: asadsfaf
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int a, b;
+for (a = 0; s[a] != '\0'; a++)
+{
+for (b = 0; accept[b] != '\0'; b++)
+{
+if (s[a] == accept[b])
+{
+return (&s[a]);
+}
+}
+}
+return (0);
+}
