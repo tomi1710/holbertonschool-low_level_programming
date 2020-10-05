b85421a126ce8eb81cfeb97c576ae2bdd0b92782
tomi1710 <2294@holbertonschool.com>
2020-10-05 13:16:34 +0000

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,14 +1,18 @@
 #include "holberton.h"
-
+/**
+ *_memset - asdsadas
+ *@s: adsaddsa
+ *@b: adwdsadwa
+ *@n: adsadwad
+ *Return: adwadsa
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-  unsigned int i = 0;
-
-  while (i < n)
-    {
-      s[i] = b;
-      i++;
-    }
-
-  return (s);
+unsigned int i = 0;
+while (i < n)
+{
+s[i] = b;
+i++;
+}
+return (s);
 }
