830d55e64e9be84eb5942280f019ac8d6a204512
tomi1710 <2294@holbertonschool.com>
2020-10-05 20:15:15 +0000

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,15 +7,14 @@
  */
 char *_strchr(char *s, char c)
 {
-int i = 0;
-while (s[i] != c)
+int i;
+for (i = 0; s[i] != '\0'; i++)
 {
-i++;
-}
-if (i < c)
+if (s[i] == c)
 {
 s = &s[i];
 return (s);
+}
 }
 return (0);
 }
