d59be600640564f973d0b1e9f10175d0abc06b27
tomi1710 <2294@holbertonschool.com>
2020-10-05 13:36:40 +0000

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *_memcpy - adsad
+ *@dest: asdasf
+ *@src: asafafs
+ *@n: asfaf
+ *Return: faefsdf
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i = 0;
+while (i < n)
+{
+dest[i] = src[i];
+i++;
+}
+return (dest);
+}
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,5 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
