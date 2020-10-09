a4b59231ba5f999f1fc3c8d8564f0f3c19f3c5d5
tomi1710 <2294@holbertonschool.com>
2020-10-09 17:59:24 +0000

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,12 +11,12 @@ int main(int argc, char *argv[])
 int mul, a, b;
 if (argc == 3)
 {
-a = atoi (argv[1]);
-b = atoi (argv[2]);
-mul = a * b;
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+mul = a *b;
 printf("%d\n", mul);
 }
 else
 printf("Error\n");
-return(0);
+return (0);
 }
