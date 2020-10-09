8464ced3bb522937f71cbf359e50270da749254f
tomi1710 <2294@holbertonschool.com>
2020-10-09 19:09:02 +0000

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,46 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ *main - adsadads
+ *@argc: adasda
+ *@argv: asdadas
+ *Return:asdasdd
+ */
 int main(int argc, char *argv[])
 {
-  int coins, amount = 0;
-
-  if (argv[1] < 0)
-    {
-      printf("0");
-      return (
-  if (argc != 2)
-    {
-      printf("Error\n");
-      return(1);
-    }
-  coins = atoi(argv[1]);
-
-  while (coins >= 25)
-    {
-      coins = coins - 25;
-      amount++;
-    }
-  while (coins >= 10)
-    {
-      coins = coins - 10;
-      amount++;
-    }
-  while (coins >= 5)
-    {
-      coins = coins - 5;
-      amount++;
-    }
-  while (coins >= 2)
-    {
-      coins = coins - 2;
-      amount++;
-    }
-  while (coins >= 1)
-    {
-      coins = coins - 1;
-      amount++;
-    }
-  printf("%d\n", amount);
-  return(0);
+int coins, amount = 0;
+coins = atoi(argv[1]);
+if (coins < 0)
+{
+printf("0");
+return (0);
+}
+if (argc != 2)
+{
+printf("Error\n");
+return(0);
+}
+while (coins >= 25)
+{
+coins = coins - 25;
+amount++;
+}
+while (coins >= 10)
+{
+coins = coins - 10;
+amount++;
+}
+while (coins >= 5)
+{
+coins = coins - 5;
+amount++;
+}
+while (coins >= 2)
+{
+coins = coins - 2;
+amount++;
+}
+while (coins >= 1)
+{
+coins = coins - 1;
+amount++;
+}
+printf("%d\n", amount);
+return(0);
 }
