aa5dffe858f2ef456e6e20551b00559807ee7a98
tomi1710 <2294@holbertonschool.com>
2020-10-09 18:55:11 +0000

    4-add.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -5,6 +5,15 @@ int main(int argc, char *argv[])
 {
   int coins, amount = 0;
 
+  if (argv[1] < 0)
+    {
+      printf("0");
+      return (
+  if (argc != 2)
+    {
+      printf("Error\n");
+      return(1);
+    }
   coins = atoi(argv[1]);
 
   while (coins >= 25)
@@ -33,3 +42,5 @@ int main(int argc, char *argv[])
       amount++;
     }
   printf("%d\n", amount);
+  return(0);
+}
