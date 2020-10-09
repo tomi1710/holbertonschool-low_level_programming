c6b7717c505f2846bc7c9e9469ceaef7277c64da
tomi1710 <2294@holbertonschool.com>
2020-10-09 14:52:09 +0000

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - asdads
+ *@argc: asdasda
+ *@argv: asdadads
+ *Return: asdads
+ */
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; argc > i; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
