7612d2a502bae1a87cb03a1870ce4446bcc74236
tomi1710 <2294@holbertonschool.com>
2020-10-10 17:46:42 +0000

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,10 +9,14 @@
 int main(int argc, char *argv[])
 {
 int i, sum = 0, a;
+if (argc == 1)
+{
+printf("0\n");
+}
 for (i = 1; i < argc; i++)
 {
 a = atoi(argv[i]);
- if ((a == 0 && *argv[i] != '0') || (a == 0 && *argv[i] != "00"))
+if (a == 0 && *argv[i] != '0')
 {
 printf("Error\n");
 return (1);
