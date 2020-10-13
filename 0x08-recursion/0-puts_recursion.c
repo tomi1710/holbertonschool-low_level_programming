f8fa96018b94266d4f71ccf87aa7ee737f4a3a28
tomi1710 <2294@holbertonschool.com>
2020-10-13 12:31:11 +0000

    a

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,17 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  *_puts_recursion - asdadasda
  *@s: asadsadsasd
  */
 void _puts_recursion(char *s)
 {
-_putchar(s[0]);
 if (s[0] == '\0')
 {
 _putchar('\n');
 }
 else
 {
-printf(s[0]);
+_putchar(s[0]);
 _puts_recursion(s + 1);
 }
 }
