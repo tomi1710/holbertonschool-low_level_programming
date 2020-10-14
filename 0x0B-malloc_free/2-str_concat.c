a9f88472a0f3950f1c59de1ce4c437a0f267b9a5
tomi1710 <2294@holbertonschool.com>
2020-10-14 17:55:14 +0000

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,10 +10,10 @@ char *str_concat(char *s1, char *s2)
 {
 int i = 0, i2 = 0, i3;
 char *p;
-for (i = 0; s1[i] != '\0'; i++)
+for (; s1[i] != '\0'; i++)
 {
 }
-for (i2 = 0; s2[i2] != '\0'; i2++)
+for (; s2[i2] != '\0'; i2++)
 {
 }
 i2 = i2 + i;
