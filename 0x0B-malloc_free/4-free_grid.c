6ded1b285b8227a82da4b0a8708362ada93d1eb4
tomi1710 <2294@holbertonschool.com>
2020-10-14 20:38:27 +0000

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * free_grid - generates matrix
+ * @grid: width of matrix
+ * @height: height of matrix
+ * Return: pointer to matrix
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+}
