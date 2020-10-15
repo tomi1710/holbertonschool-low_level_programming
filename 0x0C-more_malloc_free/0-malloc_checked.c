a611db6ff45e67ecf374dce8693ed94028cf8b50
tomi1710 <2294@holbertonschool.com>
2020-10-15 15:30:10 +0000

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,16 @@
 #include "holberton.h"
 #include <stdlib.h>
-
+/**
+ *malloc_checked - checks if malloc is possible
+ *@b: amount of bytes
+ *Return: pointer to memory
+ */
 void *malloc_checked(unsigned int b)
 {
 	char *p;
-	p = malloc(b);
 
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
 	return (p);
 }
