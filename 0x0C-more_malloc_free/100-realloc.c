202366f90fa114a8944f26f48e80aee4619caaa3
tomi1710 <2294@holbertonschool.com>
2020-10-15 20:00:08 +0000

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,8 +14,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
-	if (new_size > old_size)
-		new_size = old_size;
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
@@ -25,7 +23,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	p = malloc(new_size);
 	if (p == NULL)
 		return (NULL);
-
+	if (new_size > old_size)
+		new_size = old_size;
 	for (i = 0; i < new_size; i++)
 	{
 		((char *)p)[i] = ((char *)ptr)[i];
