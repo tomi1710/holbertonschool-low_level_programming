c591e149f9c96b62f9fa962d3f51a5b53ae0e945
tomi1710 <2294@holbertonschool.com>
2020-10-19 16:51:00 +0000

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *init_dog - adadsd
+ *@d: asdasd
+ *@name: asdad
+ *@age: asda
+ *@owner: adassd
+ *Return: asdad
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+	struct dog dog;
+
+	dog.name = name;
+	dog.age = age;
+	dog.owner = owner;
+}
+#endif
