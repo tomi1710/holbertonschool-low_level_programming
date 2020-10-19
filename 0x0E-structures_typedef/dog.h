e377181a0350dcf2a932c13f678578157978ec42
tomi1710 <2294@holbertonschool.com>
2020-10-19 16:33:33 +0000

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *struct dog - adads
+ *@name: asdasd
+ *@age: asdads
+ *@owner: asdad
+ */
+struct dog
+{
+        char* name;
+        float age;
+        char* owner;
+};
