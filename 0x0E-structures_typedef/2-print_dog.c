95b527414c41ed89ac099df92e4c4c34d8ee0375
tomi1710 <2294@holbertonschool.com>
2020-10-19 17:27:01 +0000

    0x0E-structures_typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,14 +16,6 @@ void print_dog(struct dog *d)
 		{
 		printf("Name: %s\n", d->name);
 		}
-/**		if (d->age == NULL)
-		{
-			printf("Age: (nil)");
-		}
-		else
-		{
-		printf("Age: %f\n", d->age);
-		}*/
 		printf("Age: %f\n", d->age);
 		if (d->owner == NULL)
 		{
