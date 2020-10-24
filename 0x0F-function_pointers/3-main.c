26a67121872168b9098b7c1ded7a8d5173a974dc
tomi1710 <2294@holbertonschool.com>
2020-10-24 05:38:18 +0000

    a

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - gfgf
+ * @argc: fdsdf
+ * @argv: dfgsdfg
+ * Return: sdfsadf
+ */
+
+int main(int argc, char *argv[])
+{
+  if (argc != 4)
+    {
+      printf("Error\n");
+      exit(98);
+    }
+  if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
+    {
+      printf("Error\n");
+      exit(100);
+    }
+  printf("%d\n", (*get_op_func)(argv[2])(atoi(argv[1]), atoi(argv[3])));
+  return (0);
+
+}
