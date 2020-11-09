0f8474d46bf2787902dd9a922a3bb12e63ec8ea9
tomi1710 <2294@holbertonschool.com>
2020-11-09 18:54:48 +0000

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,9 +13,14 @@ int create_file(const char *filename, char *text_content)
 
 	int fd, len;
 
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
 	len = _strlen(text_content);
 
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 
 	if (fd == -1)
 	{
