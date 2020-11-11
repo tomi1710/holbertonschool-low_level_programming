1baf52a7206cabe6a10cdcb543a83c468fd4ac3a
tomi1710 <2294@holbertonschool.com>
2020-11-11 13:55:56 +0000

    0x10-variadic_functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,5 +1,9 @@
 #include "variadic_functions.h"
-
+/**
+ *print_numbers - prints numbers given with a specific separator
+ *@separator: separator given
+ *@n:number of numbers given
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 
