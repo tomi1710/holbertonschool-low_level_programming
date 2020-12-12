6c0fb8ec1924d3900a5a45e78376943042e84eea
tomi1710 <2294@holbertonschool.com>
2020-12-12 15:33:51 +0000

    add comments

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,14 +1,18 @@
 #include "lists.h"
-
+/**
+ *print_dlistint - prints elements of a list
+ *@h: the inital pointer to the linked list
+ *Return: returns the amount of elements in the list
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
-  int i = 0;
+	int i = 0;
 
-  while (h != NULL)
-    {
-      printf("%i\n", h->n);
-      h = h->next;
-      i++;
-    }
-  return(i);
+	while (h != NULL)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,13 +1,17 @@
 #include "lists.h"
-
+/**
+ *dlistint_len - returns the amount of elements
+ *@h: the inital pointer to the linked list
+ *Return: returns the amount of elements
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
-  int i = 0;
+	int i = 0;
 
-  while (h != NULL)
-    {
-      i++;
-      h = h->next;
-    }
-  return(i);
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
 }
