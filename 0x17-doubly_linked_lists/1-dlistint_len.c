f5905e9930c92b15322dfd8e5fcc4aeb72f058ab
tomi1710 <2294@holbertonschool.com>
2020-12-12 15:11:48 +0000

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,14 +1,13 @@
 #include "lists.h"
 
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
   int i = 0;
 
   while (h != NULL)
     {
-      printf("%i\n", h->n);
-      h = h->next;
       i++;
+      h = h->next;
     }
   return(i);
 }
