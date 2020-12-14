6e968b3aeedd2054ea392d789cd699e4266d8e9f
tomi1710 <2294@holbertonschool.com>
2020-12-14 16:56:20 +0000

    100

---
+++ 0x18-dynamic_libraries/functions_100.c
@@ -0,0 +1,60 @@
+#include "test.h"
+/**
+ * add - adds two intigers
+ * @a: first integer
+ * @b: second integer
+ * Return: return
+ */
+int add(int a, int b)
+{
+	a = a + b;
+	return (a);
+}
+
+/**
+ * sub - substracts b from a
+ * @a: first integer
+ * @b: second integer
+ * Return: return
+ */
+int sub(int a, int b)
+{
+	a = a - b;
+	return (a);
+}
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: return
+ */
+int mul(int a, int b)
+{
+	a = a * b;
+	return (a);
+}
+
+/**
+ * div - divides two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: return
+ */
+int div(int a, int b)
+{
+	a = a / b;
+	return (a);
+}
+
+/**
+ * div - divides two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: return
+ */
+int mod(int a, int b)
+{
+	a = a % b;
+	return (a);
+}
---
+++ 0x18-dynamic_libraries/test.h
@@ -0,0 +1,10 @@
+#ifndef TEST_H
+#define TEST_H
+#include <string.h>
+#include <stdio.h>
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+#endif
