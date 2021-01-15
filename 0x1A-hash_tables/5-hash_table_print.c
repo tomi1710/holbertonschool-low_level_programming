0665db688f90106e7bca9638dc46a65eb2bc9f08
tomi1710 <2294@holbertonschool.com>
2021-01-15 21:43:41 +0000

    first commit

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table to print
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *aux_node;
+	unsigned long int i;
+	char *separador = "";
+
+	if (ht)
+	{
+		printf("{");
+		for (i = 0; i < ht->size; i++)
+		{
+			aux_node = ht->array[i];
+			while (aux_node != NULL)
+			{
+				printf("%s", separador);
+				separador = ", ";
+
+				if (aux_node->key)
+					printf("'%s': '%s'", aux_node->key, aux_node->value);
+
+				aux_node = aux_node->next;
+			}
+		}
+		printf("}\n");
+	}
+}
