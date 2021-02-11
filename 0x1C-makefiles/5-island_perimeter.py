3b8946b8910b2a4c26132e4555a981ab21a0ae1b
tomi1710 <2294@holbertonschool.com>
2021-02-11 17:03:31 +0000

    add

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+returns the perimeter of the island described in grid
+"""
+
+def island_perimeter(grid):
+    per=0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                if grid[i - 1][j] == 0:
+                    per = per + 1
+                if grid[i + 1][j] == 0:
+                    per = per + 1
+                if grid[i][j - 1] == 0:
+                    per = per + 1
+                if grid[i][j + 1] == 0:
+                    per = per + 1
+    return per
