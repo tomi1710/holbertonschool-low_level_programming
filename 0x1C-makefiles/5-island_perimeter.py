2775f8a53229c771cc4190afdac991e83b17a138
tomi1710 <2294@holbertonschool.com>
2021-02-11 18:33:16 +0000

    fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,24 +7,6 @@ returns the perimeter of the island described in grid
 def island_perimeter(grid):
     """ def """
     per = 0
-    lake = 0
-
-    for i in range(len(grid) - 1):
-        for j in range(len(grid[0]) - 1):
-            lake = 0
-            if i == 0 or j == 0:
-                lake = 0
-            if grid[i][j] == 0:
-                if grid[i - 1][j] != 0:
-                    lake = lake + 1
-                if grid[i + 1][j] != 0:
-                    lake = lake + 1
-                if grid[i][j - 1] != 0:
-                    lake = lake + 1
-                if grid[i][j + 1] != 0:
-                    lake = lake + 1
-                if lake == 4:
-                    grid[i][j] = 1
 
     for i in range(len(grid)):
         for j in range(len(grid[0])):
